1ad237aba5c285b4fbe781604be9d0d522ccafeb
Ian Hancock <northroadian@gmail.com>
2021-05-14 15:49:22 -0500

    adding more to the quiz the answer section works now

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const correctAnswers = [ 'B','B','B','B']
-
 const form = document.querySelector('.quiz-form')
+const result = document.querySelector('.result')
+
 form.addEventListener('submit', event => {
     event.preventDefault()
     
@@ -11,6 +12,13 @@ userAnswers.forEach((answer,index)=> {
     if(answer === correctAnswers[index]){
         score += 25; 
     }
+    else{
+        
+    }
 })
-
+ // show the result on page. window?? 
+ result.querySelector('span').textContent =`${score}%`
+ result.classList.remove('d-none')
+ // result.classlist.remove('d-none') will go in and remove the class list.
+ // it is set to none so removing will make the divs appear.
 })
